6bd4463c6f0e6d575ad1f5c2a7ce53af0eb3b229
AndrewOskolok <oskolok7@gmail.com>
2022-07-04 08:23:30 +0300

    update

--- deploy.js
+++ deploy.js
@@ -3,26 +3,20 @@ const ftpDeploy = new FtpDeploy();
 
 const config = {
     user: "root",
-    // Password optional, prompted if none given
     password: "12039500",
     host: "109.251.233.11",
     port: 22,
     localRoot: __dirname + "/build",
     remoteRoot: "/var/www/okshimel.shop/",
-    include: ["*", "**/*", ".*"],      // this would upload everything except dot files
-
-    // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
+    include: ["*", "**/*", ".*"],
     exclude: [
         // "dist/**/*.map",
         // "node_modules/**",
         // "node_modules/**/.*",
         // ".git/**",
     ],
-    // delete ALL existing files at destination before uploading, if true
     deleteRemote: true,
-    // Passive mode is forced (EPSV command is not sent)
     forcePasv: false,
-    // use sftp or ftp
     sftp: true,
 };
 

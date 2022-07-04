4db4cac92c6ed12ceb4c9aa17d993e411771d658
AndrewOskolok <oskolok7@gmail.com>
2022-07-04 14:14:19 +0300

    auto deploy to github

--- src/redux/reducers/loaderReducer.js
+++
@@ -1,9 +0,0 @@
-import { createReducer } from "@reduxjs/toolkit";
-import { loaderOn, loaderOff } from "../actions/loaderAction";
-
-const initialState = false;
-
-export default createReducer(initialState, {
-  [loaderOn]: () => true,
-  [loaderOff]: () => false,
-});

c02558c22a7cd003ec52620ff3082c0f17321172
AndrewOskolok <oskolok7@gmail.com>
2022-09-03 15:30:04 +0300

    update back url

--- src/Components/Logotype/Logotype.jsx
+++ src/Components/Logotype/Logotype.jsx
@@ -8,7 +8,7 @@ const Logotype = () => {
   return (
     <h1 className={css.logo}>
       <Link to="/" className={css.logo__link}>
-        Okshimel
+        Okshimel test
         <img
           className={css.logo__icon}
           src={owner}
